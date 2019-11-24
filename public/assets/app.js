5f32039700c757da0b635c424ff74a165429164a
Bitter Starglow <freym827@gmail.com>
2019-11-23 21:54:48 -0500

    asdfads

--- public/assets/app.js
+++ public/assets/app.js
@@ -17,13 +17,12 @@ gapi.load('auth2', function() {
 })
 
 function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    // var profile = googleUser.getBasicProfile();
+    // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    // console.log('Name: ' + profile.getName());
+    // console.log('Image URL: ' + profile.getImageUrl());
+    // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
     var id_token = googleUser.getAuthResponse().id_token
-    console.log(id_token)
     var xhr = new XMLHttpRequest();
     xhr.open('POST', '/api/tokensignin');
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
