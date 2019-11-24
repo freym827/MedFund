924b57b039a7e065081c2dbc5f521fd325cbd4ce
Bitter Starglow <freym827@gmail.com>
2019-11-24 17:39:09 -0500

    afsdifhiawuhef

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,3 +1,23 @@
+function onSuccess(googleUser) {
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+}
+function onFailure(error) {
+console.log(error);
+}
+
+function renderButton() {
+    gapi.signin2.render('my-signin2', {
+      'scope': 'profile email',
+      'width': 240,
+      'height': 50,
+      'longtitle': true,
+      'theme': 'dark',
+      'onsuccess': onSuccess,
+      'onfailure': onFailure
+    });
+}
+renderButton()
+
 
 gapi.load('auth2', async function(){
     auth2 = gapi.auth2.init({
