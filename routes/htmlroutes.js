cda88265d5256376376f7a8fa492f7a5d3746954
Bitter Starglow <freym827@gmail.com>
2019-11-24 16:31:06 -0500

    Asdgwe

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -2,7 +2,7 @@ var path = require("path");
 
 module.exports = function(app) {
 
-  app.get("/signin", function(req, res) {
+  app.get("/mymedfund", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/mymedfund.html"));
   });
 
