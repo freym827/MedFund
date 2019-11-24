da01fb83f1e60c99400ffc01bbbe330622d50b80
Bitter Starglow <freym827@gmail.com>
2019-11-23 20:08:13 -0500

    signin trying

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -2,6 +2,10 @@ var path = require("path");
 
 module.exports = function(app) {
 
+  app.get("/signin", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/signin.html"));
+  });
+
   app.get("/profile", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/profile.html"));
   });
