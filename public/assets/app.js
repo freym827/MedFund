5fe3c04b1a24500b53cafeb94e2741209d874f14
Bitter Starglow <freym827@gmail.com>
2019-11-24 00:20:07 -0500

    awtreavas

--- public/assets/app.js
+++ public/assets/app.js
@@ -5,20 +5,12 @@ var initClient = function() {
     gapi.load('auth2', function(){
         auth2 = gapi.auth2.init({
             client_id: '7917026339-nv3kftq6gd34gr0ipegnjitujib77c4j.apps.googleusercontent.com'
-        });
-        if (auth2.isSignedIn.get()) {
-            var profile = auth2.currentUser.get().getBasicProfile();
-            console.log('ID: ' + profile.getId());
-            console.log('Full Name: ' + profile.getName());
-            console.log('Given Name: ' + profile.getGivenName());
-            console.log('Family Name: ' + profile.getFamilyName());
-            console.log('Image URL: ' + profile.getImageUrl());
-            console.log('Email: ' + profile.getEmail());
-        }else {
-            console.log('NOPE')
-        }
-    });
-};
+        })
+    })
+}
+
+initClient()
+console.log(auth2)
 
 initClient()
 var dropList = () => {
