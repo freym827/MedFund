a8ec91477270b7ac7da1e62d89c46dddd2024683
Bitter Starglow <freym827@gmail.com>
2019-11-23 21:44:59 -0500

    asdfa

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,4 +1,21 @@
+const {OAuth2Client} = require('google-auth-library');
 const connection = require('../config/connection')
+const client = new OAuth2Client('7917026339-nv3kftq6gd34gr0ipegnjitujib77c4j.apps.googleusercontent.com');
 module.exports = function(app) {
-   
+   app.post('/api/tokensignin', (req, res) => {
+      verify().catch(console.error);
+   })
+}
+
+async function verify() {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: '7917026339-nv3kftq6gd34gr0ipegnjitujib77c4j.apps.googleusercontent.com',  // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
 }
