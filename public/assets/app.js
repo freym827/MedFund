8ebdee927fa0524c3b7433e18a3779558d13a179
Bitter Starglow <freym827@gmail.com>
2019-11-23 20:43:50 -0500

    still sign in

--- public/assets/app.js
+++ public/assets/app.js
@@ -21,6 +21,7 @@ var homeScroll = () => {
 gapi.load('auth2', function() {
     gapi.auth2.init()
 })
+
 function onSignIn(googleUser) {
     console.log('hello')
     var profile = googleUser.getBasicProfile();
@@ -28,4 +29,11 @@ function onSignIn(googleUser) {
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
 }
