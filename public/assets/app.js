78195db329b8a03f2b9808406612f453079c354e
Bitter Starglow <freym827@gmail.com>
2019-11-24 00:03:51 -0500

    asdfa

--- public/assets/app.js
+++ public/assets/app.js
@@ -7,15 +7,17 @@ function init() {
     });
 }
 init()
-if (auth2.isSignedIn.get()) {
-    var profile = auth2.currentUser.get().getBasicProfile();
-    console.log('ID: ' + profile.getId());
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail());
-}
+gapi.load('auth2', function() {
+    if (auth2.isSignedIn.get()) {
+        var profile = auth2.currentUser.get().getBasicProfile();
+        console.log('ID: ' + profile.getId());
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail());
+    }
+})
 
 var dropList = () => {
     drop = document.getElementById('DropDown')
