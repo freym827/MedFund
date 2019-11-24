3ce345167f8bf6ea46ca94ef1a120e20de8665cf
Bitter Starglow <freym827@gmail.com>
2019-11-23 23:38:51 -0500

    asdfasd

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,18 +1,20 @@
 gapi.load('auth2', function() {
-    gapi.auth2.init()
-})
-
-gapi.load('auth2', function() {
     auth2 = gapi.auth2.init({
       client_id: '7917026339-nv3kftq6gd34gr0ipegnjitujib77c4j.apps.googleusercontent.com',
       fetch_basic_profile: true,
       scope: 'profile'
     });
   
-    // Sign the user in, and then retrieve their ID.
-    auth2.signIn().then(function() {
-      console.log(auth2.currentUser.get().getId());
-    });
+    
+  if (auth2.isSignedIn.get()) {
+    var profile = auth2.currentUser.get().getBasicProfile();
+    console.log('ID: ' + profile.getId());
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+  }
 });
 
 var dropList = () => {
