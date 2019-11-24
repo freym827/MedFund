ded9153f2352ccbaf56ae128493762c3804ae035
Bitter Starglow <freym827@gmail.com>
2019-11-23 23:30:24 -0500

    asdfas

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,12 +1,14 @@
-if (auth2.isSignedIn.get()) {
-    var profile = auth2.currentUser.get().getBasicProfile();
-    console.log('ID: ' + profile.getId());
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail());
-}
+gapi.load('auth2', function() {
+    gapi.auth2.init()
+})
+
+gapi.load('auth2', function() {
+    console.log('hello1')
+    if (auth2.isSignedIn.get()) {
+        console.log('hello2')
+      }
+})
+
 var dropList = () => {
     drop = document.getElementById('DropDown')
     if(drop.style.display === 'none' || drop.style.display === '') {
