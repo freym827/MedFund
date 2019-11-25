321c4d773d0e65f09868eff99473d6f6ce7893ae
Bitter Starglow <freym827@gmail.com>
2019-11-25 15:05:21 -0500

    fasetase

--- public/assets/app.js
+++ public/assets/app.js
@@ -81,14 +81,14 @@ async function onSignIn(googleUser) {
     document.getElementById('SignInBox').style.display = 'none'
     // document.getElementById('ProfileLink').style.display = 'inline-block'
     // document.getElementById('SignInLink').style.display = 'none'
-    var profile = googleUser.getBasicProfile();
     // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     // console.log('Name: ' + profile.getName());
-    console.log(profile.getImageUrl());
     // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
     var profile = await googleUser.getBasicProfile();
     const name = await profile.getName()
     const email = await profile.getEmail()
+    var img = await profile.getImageUrl()
+    document.getElementById('ProfilePic').setAttribute('src', img)
     var id_token = googleUser.getAuthResponse().id_token
     var xhr = new XMLHttpRequest();
     xhr.open('POST', '/api/tokensignin');
