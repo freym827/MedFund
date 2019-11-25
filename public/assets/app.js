265f17c966916075f5d0895bb2b87e9f88389266
Bitter Starglow <freym827@gmail.com>
2019-11-24 23:18:50 -0500

    afsdfjkasdf

--- public/assets/app.js
+++ public/assets/app.js
@@ -118,7 +118,7 @@ const testdatabase = () => {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', '/api/users');
     xhr.onload = function() {
-        console.log(xhr.responseText.users);
+        console.log(xhr.responseText);
     };
     xhr.send();
 }
