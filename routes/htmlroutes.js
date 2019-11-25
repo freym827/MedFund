73fcab324069a472dc2a77f9c5c5d542e5382dba
Bitter Starglow <freym827@gmail.com>
2019-11-25 15:18:15 -0500

    fasefase

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,6 +1,10 @@
 var path = require("path");
 
 module.exports = function(app) {
+  
+  app.get("/search", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/search.html"));
+  });
 
   app.get("/mymedfund", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/mymedfund.html"));
