1faced98cec77f3b4a3d8af7b080f00506bd01dd
Bitter Starglow <freym827@gmail.com>
2019-11-24 20:08:15 -0500

    fasefaew

--- public/assets/app.js
+++ public/assets/app.js
@@ -86,8 +86,8 @@ function onSignIn(googleUser) {
 }
 
 function signOut() {
-    document.getElementById('MyMedFund').style.display = 'none'
-    document.getElementById('SignIn').style.display = 'inline-block'
+    document.getElementById('MyMedFundLink').style.display = 'none'
+    document.getElementById('SignInLink').style.display = 'inline-block'
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
